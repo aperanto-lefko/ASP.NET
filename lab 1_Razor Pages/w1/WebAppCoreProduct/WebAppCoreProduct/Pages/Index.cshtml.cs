0e9d1e3dfd5f557bfa753b86a3fd9c52daace1e6
aperanto-lefko <polarster@yandex.ru>
2025-03-18 12:55:04 +0300

    Создание репозитория

---
+++ lab 1_Razor Pages/w1/WebAppCoreProduct/WebAppCoreProduct/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Xml.Linq;
+
+namespace WebAppCoreProduct.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+        public bool IsCorrect { get; set; } = true;
+        public void OnGet(string name, decimal? price)
+        {
+            if (price == null || price < 0 || string.IsNullOrEmpty(name))
+            {
+                IsCorrect = false;
+                return;
+            }
+            Price = price;
+            Name = name;
+        }
+    }
+}
