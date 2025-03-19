f4a3f9aab6f87007f150c41d338ec8aaf83c13b8
aperanto-lefko <polarster@yandex.ru>
2025-03-19 09:39:43 +0300

    Создание работы 4

---
+++ lab 1_Razor Pages/w4/WebAppCoreProduct/WebAppCoreProduct/Pages/Product.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebAppCoreProduct.Models;
+
+namespace WebAppCoreProduct.Pages
+{
+    public class ProductModel : PageModel
+    {
+		public Product Product { get; set; }
+		public string? MessageRezult { get; private set; }
+		public void OnGet()
+		{
+			MessageRezult = "Для товара можно определить скидку";
+		}
+		public void OnPost(string name, decimal? price)
+		{
+			Product = new Product();
+			if (price == null || price < 0 || string.IsNullOrEmpty(name))
+			{
+				MessageRezult = "Переданы некорректные данные. Повторите ввод";
+				return;
+			}
+			var result = price * (decimal?)0.18;
+			MessageRezult = $"Для товара {name} с ценой {price} скидка получится {result}";
+			Product.Price = price;
+			Product.Name = name;
+		}
+	}
+}
