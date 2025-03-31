f387c0a83e5bfab8419d0b3b2f11d73344f62d92
aperanto-lefko <polarster@yandex.ru>
2025-03-31 23:54:31 +0300

    Создание работы 6

---
+++ lab 6_MVC/w1/MvcCreditApp/MvcCreditApp/Models/Credit.cs
@@ -0,0 +1,14 @@
+﻿namespace MvcCreditApp.Models
+{
+	public class Credit
+	{
+		public virtual int CreditId { get; set; }
+		public virtual string Head { get; set; }
+		// Период, на который выдается кредит
+		public virtual int Period { get; set; } 
+		// Максимальная сумма кредита
+		public virtual int Sum { get; set; } 
+		// Процентная ставка
+		public virtual int Procent { get; set; }
+	}
+}
---
+++ lab 6_MVC/w2-w3/MvcCreditApp/MvcCreditApp/Models/Bid.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcCreditApp.Models
+{
+	public class Bid
+	{
+		// ID заявки
+		public virtual int BidId { get; set; }
+        // Имя заявителя
+        [DisplayName("Имя заявителя")]
+        [Required]
+        public virtual string Name { get; set; }
+        // Название кредита
+        [DisplayName("Название кредита")]
+        [Required]
+        public virtual string CreditHead { get; set; }
+        // Дата подачи заявки
+        [DisplayName("Дата подачи заявки")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yy}")]
+        public virtual DateTime bidDate { get; set; }
+	}
+}
