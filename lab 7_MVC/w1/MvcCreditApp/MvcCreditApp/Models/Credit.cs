fa4ffc5caa22f54eab2829610756c303cea7306b
aperanto-lefko <polarster@yandex.ru>
2025-04-01 23:05:49 +0300

    Создание работы 7

---
+++ lab 7_MVC/w1/MvcCreditApp/MvcCreditApp/Models/Credit.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcCreditApp.Models
+{
+	public class Credit
+	{
+		public virtual int CreditId { get; set; }
+        [DisplayName("Название")]
+        public virtual string Head { get; set; }
+        // Период, на который выдается кредит
+        [DisplayName("Период")]
+        [Required]
+        public virtual int Period { get; set; }
+        // Максимальная сумма кредита
+        [DisplayName("Сумма кредита")]
+        [Required]
+        public virtual int Sum { get; set; }
+        // Процентная ставка
+        [DisplayName("Процентная ставка")]
+        [Required]
+        public virtual int Procent { get; set; }
+	}
+}
